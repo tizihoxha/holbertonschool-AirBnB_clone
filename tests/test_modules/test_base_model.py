b8cf886040ef247e72bafa0d59d66483a8aa640d
tizihoxha <tizihoxha@gmail.com>
2022-10-10 13:37:16 -0700

    added

--- tests/test_modules/test_base_model.py
+++ tests/test_modules/test_base_model.py
@@ -6,10 +6,16 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     """Test class for base model"""
-    def test__str(self):
+    def test__str__(self):
         self.basemodel = BaseModel()
         output = f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
         self.assertEqual(print(self.basemodel), print(output))
+
+    def test_save(self):
+        self.basemodel = BaseModel()
+        self.basemodel.save()
+        self.assertTrue(hasattr(self.basemodel, "updated_at"))
+
 
         
 
