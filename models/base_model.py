bc32b385d5b95a60f134ae3fe630ef31f22820d7
tizihoxha <tizihoxha@gmail.com>
2022-10-10 04:29:47 -0700

    added

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Task 3"""
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Created base class"""
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return (F"[self.__class__.__name__] (self.id), self.__dict")
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
