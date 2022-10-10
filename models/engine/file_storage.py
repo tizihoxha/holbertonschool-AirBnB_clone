5e77cbf8551107f4ca3f34ddc2ec3ef677c3491a
tizihoxha <tizihoxha@gmail.com>
2022-10-10 06:13:08 -0700

    added

---
+++ models/engine/file_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Task 5"""
+import json
+from models import BaseModel
+
+
+class FileStorage:
+    """File storage class created"""
+    __file_path = "file.json"
+    __objects = {}
+    """class attributes"""
+
+    def all(self)
+
+
+
+
