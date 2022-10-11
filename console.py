ffdf109dbb1d51343c7ff32d3909f3c977bdd376
eno007 <eno.gupe@gmail.com>
2022-10-11 04:44:23 -0700

    Task 6

---
+++ console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module is the entry point of the command interpreter"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class for command interpreter"""
+    prompt = '(hbnb) '
+
+    def comm_quit(self, arg):
+        """Quit command to return quit program function"""
+        return True
+
+    def comm_EOF(self, arg):
+        """EOF command to return exit program function"""
+        return True
