fb2d1adba69756067baeec4490be9e2ffda1ef03
Donaldoo <donald.kumaraku@gmail.com>
2022-10-11 07:56:58 -0700

    Task 7

--- console.py
+++ console.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """This module is the entry point of the command interpreter"""
 import cmd
+from models.base_model import BaseModel
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
     """Class for command interpreter"""
     prompt = '(hbnb) '
+    classDict = {"BaseModel": BaseModel, "User": User}
 
     def do_quit(self, arg):
         """Quit command to return quit program function"""
@@ -18,6 +21,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Empty line handeler"""
         return False
+
+    def do_create(self, arg):
+        """create a new intance of basemodel"""
+        args = arg.split()
+        if args[0] in self.classDict:
+            newEl = self.classDict.get(args[0])()
+            storage.save()
+            print(newEl.id)
+        else:
+            print("** class doesn't exist **")
+        if len(args) == 0:
+            return "** class name missing **"
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
