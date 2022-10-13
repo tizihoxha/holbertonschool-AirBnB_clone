26bd5d7502ed94fd951738c475e65253c2d1ee48
Donaldoo <donald.kumaraku@gmail.com>
2022-10-13 06:33:18 -0700

    Update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,12 +6,12 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     """Test class for base model"""
-    def test__str__(self):
+    def test_str_method(self):
         self.basemodel = BaseModel()
         output = f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
         self.assertEqual(print(self.basemodel), print(output))
 
-    def test_save(self):
+    def test_save_method(self):
         self.basemodel = BaseModel()
         self.basemodel.save()
         self.assertTrue(hasattr(self.basemodel, "updated_at"))
