d65358b6c75942117d77bf36ae4614698f05764b
tizihoxha <tizihoxha@gmail.com>
2022-10-14 06:31:37 -0700

    added

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -31,3 +31,29 @@ class TestFileStorage(unittest.TestCase):
         new_dict = obj.all()
         key = "{}.{}".format(type(new_obj).__name__, new_obj.id)
         self.assertTrue(key in new_dict)
+
+    def test_save(self):
+        obj = FileStorage()
+        new_obj = BaseModel()
+        obj.new(new_obj)
+        new_dict = obj.all()
+        obj.save()
+        obj.reload()
+        dict2 = obj.all()
+        for key in new_dict:
+            key1 = key
+        for key in dict2:
+            key2 = key
+        self.assertEqual(new_dict[key1].to_dict(), dict2[key2].to_dict())
+
+    def test_reload(self):
+        with open("test.json", "w") as f:
+            obj = FileStorage()
+            new_obj = BaseModel()
+            obj.new(new_obj)
+            obj.save()
+            dict1 = obj.all()
+        os.remove("test.json")
+        obj.reload()
+        dict2 = obj.all()
+        self.assertEqual(dict1, dict2)
