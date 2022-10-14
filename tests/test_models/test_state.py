4e75175e47ca4f05c74d91e6a301858839f42576
eno007 <eno.gupe@gmail.com>
2022-10-14 04:10:31 -0700

    Update

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -4,8 +4,3 @@ from models.state import State
 from models.base_model import BaseModel
 import unittest
 
-class TestStateClass(unittest.TestCase):
-    """unitest module testing for state class"""
-    def test_state(self):
-        s = State()
-        self.assertTrue(isinstance(s, BaseModel))
