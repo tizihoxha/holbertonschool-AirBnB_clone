82608f74cdc81a34c9419eb703db601c3c978822
eno007 <eno.gupe@gmail.com>
2022-10-14 03:11:08 -0700

    update test cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,12 +9,21 @@ from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
 
-    """def test__file_path(self):
-        f = FileStorage()
-        self.assertTrue(hasattr(f, self.file_path))"""
+    def test__file_path(self):
+        f = FileStorage._FileStorage__file_path
+        self.assertEqual(str, type(f))
 
     def test_save(self):
         """ Tests the save method for filestorage """
         my_obj = BaseModel()
         Storage.save()
         self.assertTrues(os.path.exists("file.json"))
+
+    def test_instances(self):
+        storage = FileStorage()
+        self.assertIsInstance(storage, FileStorage)
+
+    def test_object(self):
+        obj_dict = FileStorage._FileStorage__objects
+        self.assertEqual(dict, type(obj_dict))
+
