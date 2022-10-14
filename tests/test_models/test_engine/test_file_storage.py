f9a82fb3979f1717c6b112b37c4a92237b033bd0
eno007 <eno.gupe@gmail.com>
2022-10-14 02:25:29 -0700

    Task 6

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,5 +3,12 @@ import models
 import unittest
 from models.user import User
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
+
+class TestFileStorage(unittest.TestCase):
+
+    def test__file_path(self):
+        f = FileStorage()
+        self.assertTrue(hasattr(self.f, __file_path))
 
