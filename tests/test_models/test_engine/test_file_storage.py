53230ac8912f7b6cd65ee1393fed2a4b5b94acf2
eno007 <eno.gupe@gmail.com>
2022-10-14 04:11:42 -0700

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,10 +9,6 @@ from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
 
-    def test__file_path(self):
-        f = FileStorage._FileStorage__file_path
-        self.assertEqual(str, type(f))
-
     def test_save(self):
         """ Tests the save method for filestorage """
         my_obj = BaseModel()
